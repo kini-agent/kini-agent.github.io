cae1cd9fc512d5eb366aa9966cda1b45e6dbce1b
Kini <kiniinsight@gmail.com>
2026-08-24 16:35:55 +0900

    Rebrand Linux installer to KINI

--- setup-linux.sh
+++ setup-linux.sh
@@ -9,7 +9,7 @@ fi
 TMP="$(mktemp)"
 trap 'rm -f "$TMP"' EXIT
 
-printf '[INFO] DEVOS Linux installer\n'
+printf '[INFO] KINI Linux installer\n'
 printf '[INFO] 공통 설치 스크립트를 다운로드합니다.\n'
 curl -fsSL https://kini-agent.github.io/setup.sh -o "$TMP"
 chmod +x "$TMP"
